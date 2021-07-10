c409192401e00bfc183bafdaafa964e253ec376d
zombieyang <zombieyang@tencent.com>
2021-07-10 15:34:18 +0800

    test-project

---
+++ test-project/Assets/Tests/Haha.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class Haha
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void HahaSimplePasses()
+        {
+            // Use the Assert class to test conditions
+            Assert.True(true);
+        }
+    }
+}
